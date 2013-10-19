eb57186626c0dbdfa8c18ed8e95583e8f0e58586
Matthew Cocci <mcocci@MCocci.(none)>
2013-10-19 13:30:19 -0400

    Added an unvec function to undo the vec operator

--- vec.R
+++ vec.R
@@ -2,6 +2,20 @@
 ## Code for applying the vec operator to a matrix
 
 vec <- function(A){
-    return cbind(as.vector(A))
+    return(cbind(as.vector(A)))
+}
+
+# Unstacks the vec operator into a matrix
+#   Assumes vecA is a column matrix
+unvec <- function(vecA, rows){
+
+    # Rows is the number of rows in the matrix
+    if(nrow(vecA) %% rows != 0){
+	return("Length of vecA not evenly divisible
+	       by given rows")
+    } else {
+	A <- matrix(vecA, rows, nrow(vecA)/rows)
+	return(A)
+    }
 }
 
