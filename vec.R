b4ffdc6d224e88b7d035fe712a5e0fd392155b59
Matthew Cocci <mcocci@MCocci.(none)>
2013-10-18 23:14:42 -0400

    Fixed and simplified vec operator

--- vec.R
+++ vec.R
@@ -2,12 +2,6 @@
 ## Code for applying the vec operator to a matrix
 
 vec <- function(A){
-    n <- ncol(A)
-
-    toRet <- rep(0, 0)
-    for(i in 1:n){  
-	toRet <- c(toRet, A[,i]
-    }
-    return toRet
+    return cbind(as.vector(A))
 }
 
