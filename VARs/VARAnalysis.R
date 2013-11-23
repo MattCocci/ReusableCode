7624e4b16ef8f18a1de1fbe2be11c4d93686b5e2
Matthew Cocci <matthew.cocci@gmail.com>
2013-11-23 01:51:04 -0500

    Commit for solving the Bellman Equation

--- VARs/VARAnalysis.R
+++ VARs/VARAnalysis.R
@@ -6,6 +6,12 @@
     # Example of VAR analysis
     # 
 
+## IMPORT OTHER FUNCTIONS ######################################
+
+library("MASS")
+source("../kronecker.R")
+source("../vec.R")
+
 ## IMPORT & FIX DATA ###########################################
 
 data <- read.table("data.txt", header=TRUE)
@@ -15,71 +21,83 @@ dates <- seq(from=1959.0, to=2013.25, by=0.25)
 n <- ncol(data) - 1 # number of components/variables
 
 # Log transformations
-to.log <- c("rgdp", "prices", "wages", "m1")
+to.log <- c("rgdp", "wages", "m1")
 for(i in 1:length(to.log)){
     data[to.log[i]] <- log(data[to.log[i]])
 }
 
-
 ## IMPORT & FIX DATA ###########################################
 
-# Y goes from 1+p to T+p; X goes from time 1 to T+p-1
+# Y goes from start.obs to T+start.obs-1
+# X goes from time start.obs-1 to T+start.obs-2
+# Presample is everything before start obs
 p <- 4 # lags
-bigT <- 150  # size of same, T
-last.obs <- bigT + p + 1
+bigT <- 150  # size, T
+start.obs <- 20 # Index where the response variable, Y, starts
 
 # Build matrices to use in estimation
 X <- matrix(0, bigT, n*p+1)
 Y <- matrix(0, bigT, n)
-for(i in 1:bigT){
+for(lil.t in start.obs:(start.obs+bigT-1)){
 
-    lil.x <- c(as.vector(t(data[(i+p-1):i, 2:(n+1)])),1)
-    X[i,] <- lil.x
-    Y[i,] <- as.matrix(data[i+p, 2:(n+1)])
+    lil.x <- c(as.vector(t(data[(lil.t-1):(lil.t-p), 2:(n+1)])),1)
+    X[lil.t-start.obs+1,] <- lil.x
+    Y[lil.t-start.obs+1,] <- as.matrix(data[lil.t, 2:(n+1)])
 
 }
-
 
 # Get OLS estimator
 phi.ols <- solve(t(X) %*% X) %*% t(X) %*% Y
 
-# Forecast from bigT + p + 1 onwards
-Y.forecast <- cbind(data$date, matrix(0, nrow(data), n))
-for(i in 1:nrow(data)){
-
-    # If before first forecast at time T+p+1, take historical
-    if(i <= bigT + p ){  
-
-	Y.forecast[i, 2:(n+1)] <- as.matrix(data[i, 2:(n+1)])
-
-    } else {
-
-	# Else, forecast from last three lags
-	lil.x <- c(as.vector(t(Y.forecast[(i-1):(i-p), 2:(n+1)])), 1)
-	Y.forecast[i, 2:(n+1)] <- t(phi.ols) %*% lil.x
-    }
-}
-
 
 ## OLS FORECASTS ###########################################
 
-    # Compare OLS forecasts to actual data
-    #series <- 2 # rgdp
-    #series <- 3 # unemp
-    #series <- 4 # prices
-    #series <- 5 # wages 
-    series <- 6 # m1
 
-    # Plot the forecast 
-    plot(dates, Y.forecast[,series], type="l", col="red")
+    # Forecast from bigT + p + 1 onwards
+    Y.forecast <- cbind(data$date, matrix(0, nrow(data), n))
+    for(lil.t in 1:nrow(data)){
 
-    # Plot the Actual data
-    lines(dates, data[,series], col="black")
+	# If before first forecast at time T+p+1, take historical
+	if(lil.t <= bigT + start.obs - 1){  
+
+	    Y.forecast[lil.t, 2:(n+1)] <- 
+		as.matrix(data[lil.t, 2:(n+1)])
+
+	} else {
+
+	    # Else, forecast from last three lags
+	    lil.x <- c(as.vector(
+		 t(Y.forecast[(lil.t-1):(lil.t-p), 2:(n+1)])
+		), 1)
+	    Y.forecast[lil.t, 2:(n+1)] <- t(phi.ols) %*% lil.x
+	}
+    }
+
+	
+    # Plot the data
+    titles = c("Log Real GDP", "Unemployment Rate", "Price Level",
+	       "Log Wages", "M1")
+
+    plot(dates, Y.forecast[,2], type="l", col="red", 
+	 main=titles[1], xlab="", ylab=titles[1], lwd=2)
+    lines(dates, data[,2], type="l", lwd=2)
+
+    x11()
+
+    par(mfrow = c(2, 2))
+    for(i in 1:4){
+	plot(dates, Y.forecast[,i+2], type="l", col="red", 
+	     main=titles[i+1], xlab="", ylab=titles[i+1], lwd=2)
+	lines(dates, data[,i+2], type="l", main=titles[i+1], 
+	     xlab="", ylab=titles[i+1], lwd=2)
+    }
+
+
     
-    
+## BAYESIAN FORECASTS ########################################
 
 
 
 
 
-
+#
