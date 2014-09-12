cefb88dbfbcaaa8130763b835acf8d95440f8eca
Matthew Cocci <Matthew.Cocci@ny.frb.org>
2014-09-12 17:47:26 -0400

    Time-varying handling complete; description updated and nearly complete; still need to code up output

--- Dynamics/kfilter.m
+++ Dynamics/kfilter.m
@@ -1,69 +1,75 @@
-function [L,zend,Pend,varargout] = ...
-  kfilter(data, m_fwd, C, T, R, D, M, Q, s, P, tv)
+function [L, s_end, ss_end, s_filt, ss_filt, y_prederr, varargout] = ...
+  kfilter(data, m_fwd, C, T, R, D, M, Q, s, ss, tv)
 %
 % Kalman Filter programming that accommodates:
-% 1. Missing Data: Must be marked by a NaN in the data matrix.
-%    if so, then the corresponding row in the measurement
-%    equation will be removed. 
-% 2. Time-Varying Matrices: For all arrays that define the
-%    transition and measurement equations, we first check if
-%    there is a third dimension. If yes, then that third
-%    dimension is assumed to index time. All checks are done
-%    for the matrices individually, so you could have a
-%    time-varying T, but a non-time-varying M, for example.
+% 1. Missing Data: Must be marked by a NaN in the data matrix.  if so,
+%    then the corresponding row in the measurement equation will be
+%    removed. 
+% 2. Time-Varying Matrices: For all arrays that define the transition
+%    and measurement equations, we first check if there is a third
+%    dimension. If yes, then that third dimension is assumed to index
+%    time. All checks are done for the matrices individually, so you
+%    could have a time-varying T, but a non-time-varying M, for example.
 %
-%    NOTE: A matrix in the t-th location (in the third dimension
-%    of the array) is assumed to be the matrix that is relevant
-%    for the transition equation that gets the state from time
-%    t-1 to t, or for the measurment equation that relates y_t
-%    to s_t.
+%    NOTE: A matrix in the t-th location (in the third dimension of the
+%    array) is assumed to be the matrix that is relevant for the
+%    transition equation that gets the state from time t-1 to t, or for
+%    the measurement equation that relates y_t to s_t.
 % 
-% State Space Model Represention
-%
+% State Space Model Represention %
 %   s_t = C + T*s_{t-1} + R*e_t (state or transition equation)
 %   y_t = D + M*s_t + Q*eta_t   (observation or measurement equation)
 %
 % Function arguments:
-%   data  (Ny x capT) matrix containing data (y(1),...,y(capT))'
-%   m_fwd The number of steps to forecast after the end of the
-%            data, i.e. you forecast to capT+m
-%   C     (Ns x 1) column vector representing the constant terms
-%           in the transition equation
-%   T     (Ns x Ns) transition matrix
-%   R     (Ns x Ns) covariance matrix for exogenous shocks e_t
-%           in the transition equation
-%   D     (Ny x 1) vector for constant terms in the measurement
-%           equation
-%   M     (Ny x Ns) matrix for the measurement equation.
-%   Q     (Ny x Ny) covariance matrix for the exogenous shocks
-%           eta_t in the measurment equation
-%   z is an optional Nz?1 initial state vector.
-%   P is an optional Nz?Nz covariance matrix of an initial state vector.
-%   tv    A indicator for whether or not 1 or more matrices in
-%           the state transition or measurement equation are
-%           time varying. If not, save some computational costs
+%   data    (Ny x capT) matrix containing data (y(1),...,y(capT))'
+%   m_fwd   The number of steps to forecast after the end of the data,
+%             i.e. you forecast to capT+m
+%   C       (Ns x 1) column vector representing the constant terms in
+%             the transition equation
+%   T       (Ns x Ns) transition matrix
+%   R       (Ns x Ns) covariance matrix for exogenous shocks e_t in the
+%             transition equation
+%   D       (Ny x 1) vector for constant terms in the measurement
+%             equation
+%   M       (Ny x Ns) matrix for the measurement equation.
+%   Q       (Ny x Ny) covariance matrix for the exogenous shocks eta_t
+%             in the measurment equation
+%   s       (Nz x 1) initial state vector.
+%   ss      (Nz x Nz) covariance matrix for initial state vector
+%   tv      A indicator for whether or not 1 or more matrices in the
+%           state transition or measurement equation are time varying.
+%           
 %
-%   The KALCVF function returns the following output:
-%     logl is a value of the average log likelihood function of the SSM
-%             under assumption that observation noise eps(t) is normally distributed
-%     pred is an optional Nz?(T+lead) matrix containing one-step predicted state vectors.
-%    vpred is an optional Nz?Nz?(T+lead) matrix containing mean square errors of predicted state vectors.
-%     filt is an optional Nz?T matrix containing filtered state vectors.
-%    vfilt is an optional Nz?Nz?T matrix containing mean square errors of filtered state vectors.
+% Required Output: Required because these are required for the Kalman
+% Smoother, so you'll probably want them.
 %
-%   The initial state vector and its covariance matrix of the time invariant Kalman filters
-%   are computed under the stationarity condition:
-%          z0 = (I-F)\a
-%         vz0 = (I-kron(F,F))\V(:)
-%   where F and V are the time invariant transition matrix and the covariance matrix of transition equation noise,
-%   and vec(V) is an Nz^2?1 column vector that is constructed by the stacking Nz columns of matrix V.
-%   Note that all eigenvalues of the matrix F are inside the unit circle when the SSM is stationary.
-%   When the preceding formula cannot be applied, the initial state vector estimate is set to a
-%   and its covariance matrix is given by 1E6I. Optionally, you can specify initial values.
+%   L           Likelihood, provided that the errors are normally
+%                 distributed
+%   s_end       Final filtered state vector 
+%   ss_end      Covariance matrix for the final filtered state vector
+%   s_filt      (Ns x t) matrix consisting of the filtered states
+%   ss_filt     (Ns x Ns x t) array consisting of the filtered
+%                 covariance matrices
+%   y_prederr   (Ny x t) matrix consisting of the prediction error
+%
+%
+% Optional Output: Optional because they can be used to illustrate the
+% updating procedure over time. But they aren't as important as the
+% required output.
+%
+%   y_pred      (Ny x t) matrix consisting of the y_{t+1|t}
+%   yy_pred     (Ny x Ny x t) array consisting of the covariance matrix
+%                 for y_{t+1|t}
+%   s_pred      (Ns x t) matrix consisting of the s_{t+1|t}
+%   ss_pred     (Ns x Ns x t) matrix consisting of the covariance matrix
+%                 for s_{t+1|t}
 %
 %   This is a M-file for MATLAB.
-%   Adapted from code of Iskander Karibzhanov 5-28-02.
-%==========================================================================
+%   - Adapted from kalcvf.m code of Iskander Karibzhanov 5-28-02.
+%   - Additions include a different characterization of the state
+%     transition and measurement equations, plus a way to handle
+%     time-varying matrices.
+%=======================================================================
 
   capT = size(data,2);
   Ns   = size(C,1);
@@ -92,23 +98,46 @@ function [L,zend,Pend,varargout] = ...
   if any(size(ss,1) ~= [Ns Ns])
     error('ss0 must be Ns by Ns matrix') end
   if any(R ~= diag(diag(R)))
-      error('R must be a diagonal matrix')
+    error('R must be a diagonal matrix')
   end
   if any(Q ~= diag(diag(Q)))
-      error('Q must be a diagonal matrix')
+    error('Q must be a diagonal matrix')
   end
 
-  %% Check which matrices/arrays are time-varying
+  %% Check which matrices/arrays are time-varying; Store all matrices in
+  %  structure
   if tv
-    matsnames = {'C', 'T', 'R', 'D', 'M', 'Q'};
-    matscell  = {C, T, R, D, M, Q};
-    tv_inds = find(cellfun(@(m) ndims(m) > 2, mats, ...
-                   'UniformOuput', true));
-    for mt = 1:length(matsnames)
-      mats.(matsnames{mt}) = matscell{mt};
+    mats = struct('C', C, ...
+                  'T', T, ...
+                  'R', R, ...
+                  'D', D, ...
+                  'M', M, ...
+                  'Q', Q)
+    matsnames = fieldnames(mats);
+
+    % number of elements along 3rd or t dimension
+    tdim_els  = arrayfun(@(mt) size(mats.(matsnames{mt}), 3), 1:length(matsnames));
+
+    % Which indices are time varying
+    tv_inds = find(tdim_els > 1);
+
+    % Check that 3D time-varying arrays have capT elements along 3rd dim
+    too_small = intersect(find(tdim_els ~= capT), tv_inds);
+    if ~isempty(too_small)
+      err1 = ...
+        sprintf(['The following matrices have a 3rd dimension, ' ...
+          'indicating time varying.\n However, they are not the ' ...
+          'right size, as size(mat, 3) ~= T.\n']);
+      err2 = sprintf('%s ', matsnames{too_small});
+      error([err1 err2])
     end
   end
 
+  % Pre-Allocate matrices
+  nout = nargin;
+  %s_filt = 
+
+  
   if nout>3
     pred = zeros(Nz,T);
     vpred = zeros(Nz,Nz,T);
@@ -124,10 +153,21 @@ function [L,zend,Pend,varargout] = ...
  
   % Log likelihood
   L = 0;
+
+  % Set up anonymous function that can do assignment of the time t
+  % values. This will be useful when we want to assign T_t (the time t
+  % transition matrix) to the simpler name T. We do this because T*s is
+  % way more readable than T(:,:,t)*s everywhere for all matrices in the
+  % procedure; plus, now we don't have to set up 3D matrices for
+  % non-time-varying stuff
+  asgn = @(variable,val,t) assignin('caller', variable, val(:,:,t))
   
   for t = 1:capT
 
-    
+    % If time varying, assign out the time time t matrix
+    for mt = tv_inds
+      asgn(matsnames{mt}, idx_t(mats.matsnames{mt}), t)
+    end
 
     %% Handle missing observations
       
@@ -149,10 +189,11 @@ function [L,zend,Pend,varargout] = ...
     yy = M_t*ss*M_t' + Q_t; % Var_t[y_{t+1}]
 
     %% Save forecasts
+    
     s_pred(:,t)    = s;
-    s_vpred(:,:,t) = ss;
+    ss_pred(:,:,t) = ss;
     y_pred(:,t)    = y;
-    y_vpred(:,:,t) = yy;
+    yy_pred(:,:,t) = yy;
 
     %% Evaluate the likelihood p(y_t | I_{t-1},C,T,Q,D,M,R)
     err = data_t - y;     
@@ -167,7 +208,7 @@ function [L,zend,Pend,varargout] = ...
     %% Save filtering information
     y_prederr(:,t) = err;
     s_filt(:,t)    = s;
-    s_vfilt(:,:,t) = ss;
+    ss_filt(:,:,t) = ss;
     
   end
   s_end = s;
