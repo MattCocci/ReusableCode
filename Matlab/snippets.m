fc0aafd646c57427fc15bd70c867d78b801a0597
MattCocci <matthew.cocci@gmail.com>
2014-12-20 16:37:31 -0500

    Update snipps with nanmean and nanstd

--- Matlab/snippets.m
+++ Matlab/snippets.m
@@ -1,10 +1,19 @@
-% NaN out a series, x (column vec), up until we hit the nth elelemt of x
+% Functional Programming Constructs
+map = @(fcns, vals) cellfun(@(f) f(vals{:}), fcns);
+mapc = @(fcns, vals) cellfun(@(f) f(vals{:}), fcns, 'UniformOutput', false);
+iif = @(varargin) varargin{2*find([varargin{1:2:end}], 1, 'first')}(); 
+paren = @(x, varargin) x(varargin{:});
+curly = @(x, varargin) x{varargin{:}};
+
+% NaN operations
+nanmean = @(x) mean(x(~isnan(x)));
+nanstd  = @(x) std(x(~isnan(x)));
+
+% NaN out a series, x, up until we hit the nth elelement of x
 nan_before_n = @(x, n) [nan(n-1, 1); x(n:end)];
 
-% Generate a moving average from x (column vec)
-ma_gen = @(x, pds) nan_before_n(filter(ones(1, pds)/pds, 1, x), pds);
+% Generate a moving average process 
+ma_gen = @(col_vec, pds) nan_before_n(filter(ones(1, pds)/pds, 1, col_vec), pds);
 
-% Generate a matrix of lags
-% d is a column vector; generate all lags up to n lags
-% From there, you can subset what's returned to get particular lags  
+% Generate a matrix of lags; d is a column vector, n is how far to lag it
 lag_mat = @(d, nlags) [nan(1,nlags); toeplitz(d(1:end-1), [d(1), nan(1, nlags-1)])]
