e5958e552db9570b363010696689ddb0b96d742a
Matthew Cocci <Matthew.Cocci@ny.frb.org>
2015-04-14 13:57:02 -0400

    Pre-cython commit

--- Python/Test.py
+++ Python/Test.py
@@ -1,9 +1,10 @@
+import pyximport; pyximport.install()
 import sympy as sp
-execfile("DiscretizeSDE.py")
+import SDE
 
-alpha, X, sigma = sp.symbols("alpha X sigma")
-b = -alpha*X
-s = sigma
+#alpha, X, sigma = sp.symbols("alpha X sigma")
+#b = -alpha*X
+#s = sigma
 
-D = DiscretizeSDE(b, s)
-X = D.SimulatePaths(['EM'], 1, 0, 10, 0.1)
+#D = DiscretizeSDE(b, s)
+#X = D.SimulatePaths(['EM'], 1, 0, 10, 0.1)
